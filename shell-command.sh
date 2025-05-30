08fe645ac246eb607aa01f46617aa2460395affd
sangeeth012 <sangeethra1997@gmail.com>
2025-05-30 02:20:26 +0000

    this is my second commit,create a file for shell command

---
+++ shell-command.sh
@@ -0,0 +1,58 @@
+#!bin/bash 
+
+#!/bin/bash
+
+##################################
+
+Author          : XXXXXXXXXXXXX
+
+DATE            : xx-xx-xxxx
+
+Version         : V1
+
+Purpose         : Basic of  linux command
+
+###################################
+
+
+1) ls -> list file in the  directory
+
+2) cd -> change directory
+
+3) pwd -> present working directory
+
+4) mkdir -> Make new directory
+
+5) touch -> Create a new file
+
+6) vim ->  basically used to open the file ,if the file not avaliable in the directory its create a file and open the file
+
+7) cat ->  command is mainly used to read the  file
+
+8) cd ../ -> getting back to the previous diretory
+
+9) ls -ltr -> its shows the list of file  and  all  information about the  file
+
+10):wq (or) -> to save the file
+
+11) rmdir -> remove the directory only if the directory is empty
+
+12) rm-r  -> it remove the directory if the directory content  files also
+
+13) rm-rf -> force to delete
+
+14) history -> it shows what kinds of commands you used till now
+
+15) adduser -> adduser  create a home directory for the user
+
+16) useradd -> useradd not create a home directory for user
+
+17) chmod -> this command is mainly used to given the permission to the  user  as per the need
+               Three kind of premission
+                *READ *WRITE *EXECUTE
+
+18) sudo su - -> switch to the root user
+
+19) usermod -aG sudo (username) -> This command give certain permission to the user  (as of root user)
+
+20) df -h  -> printing disk space
